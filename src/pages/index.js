b18da356642a45933b34089f9bc49bc05a5c41ac
Randy Lau <randychilau@gmail.com>
2023-05-16 11:02:58 -0700

    add video to main page
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import * as React from "react"
+import meshVideo from "../assets/meshery-full-perf-720p.mp4"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <>
+  <div>Hello world!</div>
+   <video controls>
+      <source src={meshVideo} type="video/mp4" />
+    </video>
+  </>
+  )
 }
